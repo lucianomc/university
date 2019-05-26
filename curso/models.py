6c7f3cb96b8acd0c6a6fc61196df8682b735b192
lucianomc <luu.mcosta@gmail.com>
2019-05-26 16:26:57 -0300

    Tabela e views para cursos funcionando

--- curso/models.py
+++ curso/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Course(models.Model):
     idCourse = models.AutoField(primary_key=True)
     nameCourse = models.CharField(max_length=45)
-    descriptionCourse = models.CharField(max_length=128)
+    descriptionCourse = models.TextField()
 
     def __str__(self):
         return self.nameCourse
--- curso/views.py
+++ curso/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'curso.html')
+    college = Course.objects.all()
+    context = {'allCourses': college}
+    return render(request, 'curso.html', context)
