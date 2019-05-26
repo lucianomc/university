a083dddf5af29de67edff4cb6aec7c58f4f28cd1
lucianomc <luu.mcosta@gmail.com>
2019-05-26 10:23:47 -0300

    Configurando banco de dados

--- cursos/settings.py
+++ cursos/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cursos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cursos',
+        'USER': 'reitor',
+        'PASSWORD': 'lucianomc',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
