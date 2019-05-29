92f790b0036d556532562e7bfcbbc8dd51a7812f
lucianomc <luu.mcosta@gmail.com>
2019-05-28 22:18:57 -0300

    Tudo voltando ao normal

--- curso/admin.py
+++ curso/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from .models import Course
-# from .models import Matter
-# from .models import Teacher
-# from .models import Students
-# from .models import Student_has_matter
+from .models import Matter
 
 # Register your models here.
 
 admin.site.register(Course)
-# admin.site.register(Matter)
-# admin.site.register(Teacher)
-# admin.site.register(Students)
-#admin.site.register(Student_has_matter)
+admin.site.register(Matter)
--- curso/models.py
+++ curso/models.py
@@ -9,45 +9,25 @@ class Course(models.Model):
     descriptionCourse = models.TextField()
 
     def __str__(self):
-       return self.nameCourse
+        return self.nameCourse
 
 
-# class Matter(models.Model):
-#     idMatter = models.AutoField(primary_key=True)
-#     nameMatter = models.CharField(max_length=32)
-#     descriptionMatter = models.TextField()
-#     numberStudent = models.IntegerField()
-    #course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    #teacher = models.ManyToManyField('Teacher', through='Matter_has_teacher')
+class Matter(models.Model):
+    idMatter = models.AutoField(primary_key=True)
+    nameMatter = models.CharField(max_length=32)
+    descriptionMatter = models.TextField()
+    numberStudent = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
-    #def __str__(self):
-    #    return self.nameMatter
+    def __str__(self):
+        return self.nameMatter
 
 
 # class Teacher(models.Model):
 #     idTeacher = models.AutoField(primary_key=True)
 #     nameTeacher = models.CharField(max_length=64)
-    #matter = models.ManyToManyField('Matter', through='Matter_has_teacher')
+
     # Como eu faço pra retornar só os campos de materia depois que selecionar o curso?
 
     #def __str__(self):
     #    return self.nameTeacher
-
-
-# class Matter_has_teacher(models.Model):
-#     teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE)
-#     matter = models.ForeignKey('Matter', on_delete=models.CASCADE)
-
-
-# class Students(models.Model):
-#     idStudent = models.AutoField(primary_key=True)
-#     nameStudent = models.CharField(max_length=64)
-
-#     def __str__(self):
-#         return self.nameStudent
-
-
-# class Student_has_matter(models.Model):
-#     yieldStudent = models.DecimalField(max_digits=2, decimal_places=1)
-#     student = models.ForeignKey('Students', on_delete=models.CASCADE)
-#     matter = models.ForeignKey('Matter', on_delete=models.CASCADE)
