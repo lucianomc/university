e5814050dff2bf81af8eb19800a9e72cb5a4dd31
lucianomc <luu.mcosta@gmail.com>
2019-07-04 11:11:44 -0300

    Updating

--- curso/urls.py
+++ curso/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 app_name = 'curso'
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('course/<int:courseId>/', views.description, name='description'),
     path('matter/<int:matterId>/', views.matterDesc, name='matterDesc'),
-    path('novo/', views.new_course, name='newCourse'),
+    path('novocurso/', views.new_course, name='newCourse'),
     #path('student/<int:studentId>/', views.StudentDesc, name='StudentDesc'),
 ]
 
